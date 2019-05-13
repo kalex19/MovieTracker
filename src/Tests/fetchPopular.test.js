61eed82ce2a4df87d5b729eff106e73a572aa50d
Katherine Lewis <kale8432@colorado.edu>
2019-05-13 06:06:38 -0600

    Built out testing suite for Popular fetch via thunk
    Co-authored-by: Erik Konner <erikkonner@gmail.com>

--- src/Tests/fetchPopular.test.js
+++ src/Tests/fetchPopular.test.js
@@ -1,68 +1,57 @@
-// import {
-//   fetchStaff
-// } from './fetchStaff';
-// import {
-//   fetchBios
-// } from './fetchBios';
-// import {
-//   isLoading,
-//   hasErrored,
-//   setStaff
-// } from '../actions';
+import { fetchNowPlaying } from '../Thunks/fetchNowPlaying';
+import { fetchPopular } from '../Thunks/fetchPopular';
+import { isLoading, hasErrored, getPopular } from '../actions';
 
-// jest.fn('./fetchBios.js')
+jest.mock('../Thunks/fetchNowPlaying.js');
 
-// describe('fetchStaff', () => {
-//       let mockUrl;
-//       let mockStaff;
-//       let mockDispatch;
+describe('fetchPopular', () => {
+	let mockUrl;
+	let mockDispatch;
+	let mockMovie;
+	let thunk;
 
-//       beforeEach(() => {
-//         mockUrl = 'someurl.com'
-//         mockStaff = [{
-//           name: 'Christie',
-//           info: 'some info'
-//         } {
-//           name: 'Leta',
-//           info: 'some info'
-//         }]
-//         mockDispatch = jest.fn()
-//         window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-//           ok: true,
-//           json: () => Promise.resolve({
-//             bio: mockStaff
-//           })
-//         }))
-//       })
-//       it('calls dispatch with isLoading(true)', () => {
-//         const thunk = fetchStaff(mockUrl)
-//         thunk(mockDispatch)
-//         expect(mockDispatch).toHaveBeenCalledWith(isLoading(true))
-//       })
-//       it('fetch gets called with correct params', () => {
-//         thunk(mockDispatch)
-//         expect(window.fetch).toHaveBeenCalledWith(mockUrl)
-//       })
-//       it('should dispatch hasError with a message if the response is not ok', async () => {
-//         window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-//           ok: false,
-//           statusText: 'something went wrong'
-//         }))
-//         await thunk(mockDispatch)
-//         expect(mockDispatch).toHaveBeenCalledWith(hasErrored('something went wrong'))
-//       })
-//       it('should dispatch fetchBios with correct params if the response is ok', async () => {
-//           expect(mockDispatch.toHaveBeenCalledWith(fetchBio(mockStaff))
-//           })
+	beforeEach(() => {
+		thunk = fetchPopular(mockUrl);
+		mockUrl = 'someurl.com';
+		mockDispatch = jest.fn();
+		mockMovie = [
+			{
+				vote_count: 4845,
+				id: 299534,
+				title: 'Avengers: Endgame',
+				poster_path: '/or06FN3Dka5tukK1e9sl16pB3iy.jpg',
+				original_title: 'Avengers: Endgame'
+			}
+		];
 
-//         it('should dispatch isLoading (false) if response is ok', async () => {
-//           await thunk(mockDispatch)
-//           expect(mockDispatch).toHaveBeenCalledWith(isLoading(false))
-//         })
+		window.fetch = jest.fn().mockImplementation(() =>
+			Promise.resolve({
+				ok: true,
+				json: () =>
+					Promise.resolve({
+						popular: mockMovie
+					})
+			})
+		);
+	});
 
-//         it('should dispatch setStaff', async () => {
-//           mockDispatch = jest.fn().mockImplementation(() => mockStaff)
-//           await thunk(mockDispatch)
-//           expect(mockDispatch).toHaveBeenCalledWith(setStaff(mockStaff))
-//         })
-//       }
+	it('calls dispatch with isLoading(true)', () => {
+		thunk(mockDispatch);
+
+		expect(mockDispatch).toHaveBeenCalledWith(isLoading(true));
+	});
+	it('fetch gets called with correct params', () => {
+		thunk(mockDispatch);
+		expect(window.fetch).toHaveBeenCalledWith(mockUrl);
+	});
+	it('should dispatch hasError with an error message if the response is not ok', async () => {
+		window.fetch = jest.fn().mockImplementation(() =>
+			Promise.resolve({
+				ok: false,
+				statusText: 'Something went wrong'
+			})
+		);
+		await thunk(mockDispatch);
+		expect(mockDispatch).toHaveBeenCalledWith(hasErrored('Something went wrong'));
+	});
+});
